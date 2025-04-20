ffea83dfe0b2dd50b8c2655832388b2bcad7133a
Sangita1235 <156885668+Sangita1235@users.noreply.github.com>
2025-04-21 02:22:13 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,10 +4,10 @@ $servername = "mysql_container";
 $username = "root"; 
 $password = "rootpassword"; 
 $dbname = "recipe_finder"; 
-$port = "3306"; 
 
 
-$conn = new mysqli($servername, $username, $password, $dbname,$port);
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     echo("Connection failed: " . $conn->connect_error);
 }
